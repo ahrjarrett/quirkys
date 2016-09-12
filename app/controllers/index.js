b7ca8ccb053b0a6f21213363362277afacd492c8
Andrew Jarrett <aj@Andrews-MacBook-Air.local>
2016-09-11 23:39:41 -0600

    wired css, foundation and js files to views

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,18 +1,18 @@
-var express       = require('express'),
-		router        = express.Router(),
+var express    = require('express'),
+	router       = express.Router(),
 
-    IndexData     = require('../models/index.data.js');
+	IndexData    = require('../models/index.data.js');
 
 module.exports = function(app) {
 
-    app.use('/', router);
+	app.use('/', router);
 
-    router.get('/', function(req, res) {
-        var data = new IndexData();
-        res.render('index', {
-            title: data.title,
-            url: data.url
-        });
-    });
+	router.get('/', function(req, res) {
+		var data = new IndexData();
+		res.render('index', {
+			title: data.title,
+			url: data.url
+		});
+	});
 
 };
